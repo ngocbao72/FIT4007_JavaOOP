dd6c368d31894d41890c36528c0f5240c8061fe9
Ngọc Bảo <ngocbao7205@gmail.com>
2024-09-15 16:18:01 +0700

    bai 12

---
+++ W01Ex12/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 7; i <= 100; i += 7) {
+            sum += i;
+        }
+
+        System.out.println("Tổng các số là bội số của 7 từ 1 đến 100 là: " + sum);
+    }
+}
